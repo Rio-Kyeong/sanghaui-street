42a358afa58e8c408ba2413d40b89b980b470232
KyeongWoo Ryu <80517285+RyuKyeongWoo@users.noreply.github.com>
2021-07-16 17:43:25 +0900

    Controller & Model

---
+++ java/kr/co/ss/main/controller/MainController.java
@@ -0,0 +1,27 @@
+package kr.co.ss.main.controller;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.ss.product.service.ProductService;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value = "/main/main_all.do")
+	public String mainForm(Model model) {
+		ProductService ps = new ProductService();
+		model.addAttribute("topList", ps.selectProductListTopUser());
+		
+		ProductService ps1 = new ProductService();
+		model.addAttribute("botList", ps1.selectProductListBotUser());
+		
+		return "prj3/main/main_all";
+	}
+	
+	
+}
